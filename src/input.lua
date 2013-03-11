5ea3f8328197b7a4a8920b657fc0c2c822024b43
Sheng Han Yang <shenghan.yang@gmail.com>
2013-03-11 17:12:35 +0800

    Add nexus.input.isKeyRepeat and nexus.input.isKeyTrigger functions.
     [NEW] Add nexus.input.isKeyRepeat function.
     [NEW] Add nexus.input.isKeyTrigger function.

--- src/input.lua
+++ src/input.lua
@@ -1,5 +1,8 @@
 nexus.input = {}
 
+function nexus.input.initialize()
+end
+
 function nexus.input.isKeyDown(t)
     for _, v in pairs(t) do
         if love.keyboard.isDown(v) then
@@ -18,5 +21,10 @@ function nexus.input.isKeyUp()
     return false
 end
 
-function nexus.input.initialize()
+function nexus.input.isKeyTrigger(t)
+    return false
+end
+
+function nexus.input.isKeyRepeat(t)
+    return nexus.input.isKeyDown(t) and nexus.input.isKeyTrigger(t) 
 end

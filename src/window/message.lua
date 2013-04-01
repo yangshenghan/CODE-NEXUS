48e8dde476e79e687accb3fb626cae9fbf53361e
Yang Sheng Han <shenghan.yang@gmail.com>
2013-04-01 09:57:00 +0800

    Added WindowMessage.
    [NEW] Added WindowMessage.

---
+++ src/window/message.lua
@@ -0,0 +1,56 @@
+--[[ ********************************************************************** ]]--
+--[[ [CODE NEXUS]                                                           ]]--
+--[[                                                                        ]]--
+--[[ ---------------------------------------------------------------------- ]]--
+--[[ Copyright (c) 2012-2013 CODE NEXUS Development Team                    ]]--
+--[[                                                                        ]]--
+--[[ This software is provided 'as-is', without any express or implied      ]]--
+--[[ warranty. In no event will the authors be held liable for any damages  ]]--
+--[[ arising from the use of this software.                                 ]]--
+--[[                                                                        ]]--
+--[[ Permission is granted to anyone to use this software for any purpose,  ]]--
+--[[ including commercial applications, and to alter it and redistribute it ]]--
+--[[ freely, subject to the following restrictions:                         ]]--
+--[[                                                                        ]]--
+--[[ 1. The origin of this software must not be misrepresented; you must not]]--
+--[[    claim that you wrote the original software. If you use this software]]--
+--[[    in a product, an acknowledgment in the product documentation would  ]]--
+--[[    be appreciated but is not required.                                 ]]--
+--[[                                                                        ]]--
+--[[ 2. Altered source versions must be plainly marked as such, and must not]]--
+--[[    be misrepresented as being the original software.                   ]]--
+--[[                                                                        ]]--
+--[[ 3. This notice may not be removed or altered from any source           ]]--
+--[[    distribution.                                                       ]]--
+--[[ ********************************************************************** ]]--
+
+-- / ---------------------------------------------------------------------- \ --
+-- | Import modules                                                         | --
+-- \ ---------------------------------------------------------------------- / --
+local Nexus                 = nexus
+local Core                  = Nexus.core
+local Constants             = Nexus.constants
+local Input                 = Core.import 'nexus.core.input'
+local WindowBase            = Core.import 'nexus.window.base'
+local REFERENCE_WIDTH       = Constants.REFERENCE_WIDTH
+
+-- / ---------------------------------------------------------------------- \ --
+-- | Declare object                                                         | --
+-- \ ---------------------------------------------------------------------- / --
+local WindowMessage         = {}
+
+-- / ---------------------------------------------------------------------- \ --
+-- | Member functions                                                       | --
+-- \ ---------------------------------------------------------------------- / --
+function WindowMessage.new(x, y, commands)
+    local padding = 12
+    local lineheight = 24
+    local instance = WindowBase.new(WindowMessage)
+    instance.x = 0
+    instance.y = 0
+    instance.width = REFERENCE_WIDTH
+    instance.height = 4 * lineheight + 2 * padding
+    return instance
+end
+
+return WindowMessage

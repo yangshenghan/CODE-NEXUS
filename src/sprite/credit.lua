287874a84c6924651e7be84452c454e3b6dfd9d2
Yang Sheng Han <shenghan.yang@gmail.com>
2013-04-17 17:27:02 +0800

    Improved the code quality of SpriteCredit.
    [NEW] Added SpriteCredit.pause().
    [NEW] Added SpriteCredit.resume().
    [NEW] Added SpriteCredit.setFadespeedFrames().
    [NEW] Added SpriteCredit.setTransitionFrames().
    [MOD] Changed the initial position of ScrpteCredit.
    [ENH] Improved the code quality of SpriteCredit. Now it uses coroutine
    to update.

--- src/sprite/credit.lua
+++ src/sprite/credit.lua
@@ -30,13 +30,14 @@
 local table                 = table
 local string                = string
 local ipairs                = ipairs
+local coroutine             = coroutine
 local l                     = love
 local lg                    = l.graphics
 local Nexus                 = nexus
 local Core                  = Nexus.core
 local Constants             = Nexus.constants
 local Data                  = Core.import 'nexus.core.data'
-local Grapchis              = Core.import 'nexus.core.graphics'
+local Graphics              = Core.import 'nexus.core.graphics'
 local Color                 = Core.import 'nexus.base.color'
 local Font                  = Core.import 'nexus.base.font'
 local Rectangle             = Core.import 'nexus.base.rectangle'
@@ -48,14 +49,19 @@ local REFERENCE_HEIGHT      = Constants.REFERENCE_HEIGHT
 -- | Declare object                                                         | --
 -- \ ---------------------------------------------------------------------- / --
 local SpriteCredit          = {
+    coroutines              = nil,
     lines                   = nil,
     canvas                  = nil,
     pictures                = nil,
     current                 = nil,
+    scrolling               = true,
     index                   = 0,
     limit                   = 0,
     wait                    = 0,
-    diff                    = 0
+    speed                   = 3,
+    fadespeed               = 0,
+    difference              = 0,
+    transition              = 0
 }
 
 -- / ---------------------------------------------------------------------- \ --
@@ -66,11 +72,81 @@ local function process_escaped_character(line)
     return line, Font.ALIGN_CENTER
 end
 
+local function update_credit_end(instance, dt)
+    while instance.wait > 0 do
+        if instance.picture then instance.picture[4] = instance.picture[4] - instance.difference end
+        instance.wait = instance.wait - 1
+        coroutine.yield()
+    end
+
+    while instance.opacity > 0 do
+        instance.opacity = instance.opacity - instance.fadespeed
+        coroutine.yield()
+    end
+
+    table.insert(instance.coroutines, 1, nil)
+end
+
+local function update_credit_position(instance, dt)
+    while instance.y > instance.limit do
+        instance.y = instance.y - instance.speed
+        coroutine.yield()
+    end
+    table.insert(instance.coroutines, 1, coroutine.create(update_credit_end))
+end
+
+local function update_credit_start(instance, dt)
+    while instance.opacity < 1 do
+        instance.opacity = instance.opacity + instance.fadespeed
+        coroutine.yield()
+    end
+    table.insert(instance.coroutines, 1, coroutine.create(update_credit_position))
+end
+
+local function update_picture_fadein(instance, dt, picture)
+    while picture[4] < 1 do
+        picture[4] = picture[4] + instance.transition
+        coroutine.yield(true)
+    end
+    return false
+end
+
+local function update_picture_fadeout(instance, dt, picture)
+    while picture[4] > 0 do
+        picture[4] = picture[4] - instance.transition
+        coroutine.yield(true)
+    end
+    return false
+end
+
+local function update_picture_transition(instance, dt)
+    local picture = instance.pictures[instance.index + 1]
+
+    while true do
+        if picture ~= instance.picture then
+            local transition = true
+            local fadein = coroutine.create(update_picture_fadein)
+            local fadeout = coroutine.create(update_picture_fadeout)
+
+            while transition do
+                transition = coroutine.resume(fadein, instance, dt, picture) and coroutine.resume(fadeout, instance, dt, instance.picture)
+                coroutine.yield()
+            end
+
+            picture[4] = 1
+            instance.picture[4] = 0
+            instance.picture = picture
+        end
+        coroutine.yield()
+    end
+end
+
 -- / ---------------------------------------------------------------------- \ --
 -- | Member functions                                                       | --
 -- \ ---------------------------------------------------------------------- / --
 function SpriteCredit.new(contents, limit)
     local instance = SpriteBase.new(SpriteCredit)
+    local framerate = Graphics.getFramerate()
     instance.lines = {}
 
     string.gsub(contents, '(.-)\r?\n', function(line)
@@ -83,13 +159,14 @@ function SpriteCredit.new(contents, limit)
         local width = REFERENCE_WIDTH
         local height = #instance.lines * lineheight
 
+        instance.y = (REFERENCE_HEIGHT + lineheight) * 0.5
         instance.canvas = lg.newCanvas(width, height)
         instance.rectangle = Rectangle.set(instance.rectangle, 0, 0, width, height)
 
         if limit then
             instance.limit = limit
         else
-            instance.limit = (REFERENCE_HEIGHT + lineheight) * 0.5 - height
+            instance.limit = instance.y - height
         end
 
         lg.setCanvas(instance.canvas)
@@ -100,42 +177,26 @@ function SpriteCredit.new(contents, limit)
         lg.setCanvas()
     end
 
-    instance.y = REFERENCE_HEIGHT
     instance.pictures = {}
+    instance.coroutines = {
+        coroutine.create(update_credit_start),
+        coroutine.create(update_picture_transition)
+    }
+    instance.opacity = 0
     instance.visible = true
+    SpriteCredit.setWaitingFrames(instance, framerate)
+    SpriteCredit.setFadespeedFrames(instance, framerate)
+    SpriteCredit.setTransitionFrames(instance, framerate)
     return instance
 end
 
 function SpriteCredit.update(instance, dt)
-    local picture = instance.pictures[instance.index + 1]
-
+    local sprite_update_coroutine = table.first(instance.coroutines)
+    local picture_update_coroutine = table.last(instance.coroutines)
     SpriteBase.update(instance, dt)
 
-    if instance.y > instance.limit then
-        instance.y = instance.y - 3
-    else
-        instance.wait = instance.wait - 1
-        if instance.picture then instance.picture[4] = instance.picture[4] - instance.diff end
-        if instance.wait <= 0 then
-            for index = 1, 60 do
-                instance.opacity = instance.opacity - 1 / 60
-                lg.clear()
-                Grapchis.render()
-                lg.present()
-            end
-            instance.update = function() end
-            SpriteCredit.dispose(instance)
-        end
-    end
-
-    if picture and picture[3] ~= instance.picture[3] then
-        if picture[4] ~= 1 then
-            picture[4] = picture[4] + 0.05
-            if instance.picture then instance.picture[4] = instance.picture[4] - 0.05 end
-        else
-            instance.picture = picture
-        end
-    end
+    if instance.scrolling and sprite_update_coroutine then coroutine.resume(sprite_update_coroutine, instance, dt) end
+    if picture_update_coroutine then coroutine.resume(picture_update_coroutine, instance, dt) end
 end
 
 function SpriteCredit.render(instance)
@@ -148,9 +209,25 @@ function SpriteCredit.render(instance)
     lg.draw(instance.canvas, instance.x, instance.y)
 end
 
+function SpriteCredit.pause(instance)
+    instance.scrolling = false
+end
+
+function SpriteCredit.resume(instance)
+    instance.scrolling = true
+end
+
 function SpriteCredit.setWaitingFrames(instance, frames)
     instance.wait = frames
-    instance.diff = 1 / frames
+    instance.difference = 1 / frames
+end
+
+function SpriteCredit.setFadespeedFrames(instance, frames)
+    instance.fadespeed = 1 / frames
+end
+
+function SpriteCredit.setTransitionFrames(instance, frames)
+    instance.transition = frames
 end
 
 function SpriteCredit.addPicture(instance, x, y, picture)
